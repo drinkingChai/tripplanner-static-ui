d1939e4d6ecd798dfd547043f0c1e05916dc9a8e
Wasif Zaman <teatocode@gmail.com>
2017-08-10 21:34:40 -0400

    built place table

--- models/index.js
+++ models/index.js
@@ -2,6 +2,45 @@ const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL);
 
 
+// Hotel
+// name
+// num_stars (float from 1-5)
+// amenities (string of comma delimited items)
+//
+// Activity
+// name
+// age_range (data-type string)
+//
+// Restaurant
+// name
+// cuisine (comma delimited string list)
+// price (integer from 1-5 for how many dollar signs)
+
+
+conn.define('place', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.ARRAY(Sequelize.DataTypes.FLOAT), // this array will only hold floats, location will be like 45.33, 234.33, array should have max of 2
+    allowNull: false
+  }
+})
+
+
 const sync = ()=> {
   return conn.sync({ force: true });
 }
