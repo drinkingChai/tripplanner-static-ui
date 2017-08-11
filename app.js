2e7b8efdd9808df3af73342dd02833f11833dacd
Wasif Zaman <teatocode@gmail.com>
2017-08-10 21:25:12 -0400

    express done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 const models = require('./models');
 const routes = require('./routes');
 
@@ -9,7 +12,22 @@ app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
 
+app.use(express.static(path.resolve('public')));
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
 app.use(routes);
+
+
+app.use((req, res, next)=> {
+  const error = new Error('page not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((err, req, res, next)=> {
+  res.status(err.status || 500).render('error', { error: err });
+})
 
 const port = process.env.PORT || 3000;
 
